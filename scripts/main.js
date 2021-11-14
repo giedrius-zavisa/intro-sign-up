c4f3949f1e035c02bb9ebf4d7173d111b58eed12
Sypnotick <58993512+Sypnotick@users.noreply.github.com>
2021-11-14 17:11:41 +0000

    Desktop version, Javascript functionality

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,62 @@
 const inputField = document.querySelectorAll(".input__field");
 const inputButton = document.querySelector(".input__button");
+const errorText = document.querySelectorAll(".error__text");
+const errorIcon = document.querySelectorAll(".error__icon");
 
 inputButton.addEventListener('click', function() {
-    console.log("click!");
 
     let validInput = false;
+   
+    for(let i = 0; i < inputField.length; i++) {
 
-    inputField.forEach( element => {
+         // Email validation with a regex check
+        if(i === 2) {
 
-        if(element.value.length === 0 || element.value === null || element.value === ""){
-
+            if(validateEmail(inputField[i].value)) {
+                console.log("valid email");
+                validInput = true;
+            }
+            else {
+                console.log("invalid email");
+                validInput = false;
+                invalidField(i);
+                break;
+            }
+            
         }
-        else {
+        
+        // Input field validation
+        if(inputField[i].value.length === 0 || inputField[i].value === null || inputField[i].value === ""){
+            invalidField(i);
+            validInput = false;
+            console.log("invalid field");
+            break;
+        }
+        else {           
             validInput = true;
+            console.log("all is good");
         }
-    
-    });
+    }
 
     if(validInput) {
-        console.log("point1");
+        alert("sign up complete!");
     }
-    else {
-        console.log("point2");
-    }
+
 });
+
+// Email validation via regex
+const validateEmail = function(email) {
+    var re = /^\S+@\S+\.\S+$/;
+    return re.test(email);
+}
+
+// Input field style changes when a field has wrong information
+const invalidField = function(it) {
+
+    inputField[it].style.borderColor = "red";
+    inputField[it].style.color = "red";
+    errorText[it].style.display = "block";
+    errorIcon[it].style.display = "block";
+}
+
+
